39b561a1d994f39a707f4840b89f095d467ab12b
Barbara Xavier <bxstars4@gmail.com>
2021-09-15 17:17:28 -0400

    Criada estrutura de pastas; Adicionado o Header, Nav e Footer da page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule }  from '@angular/material/toolbar';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+
+
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/template/footer/footer.component';
+import { HeaderComponent } from './shared/template/header/header.component';
+import { NavComponent } from './shared/template/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
